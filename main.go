1ac7b3be3181b8cb82d162b8f77464e5315cf48c
Adria Cidre <adria.cidre@gmail.com>
2016-08-09 11:53:13 +0200

    Ernest-config-client based service

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"runtime"
 
@@ -14,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	ecc "github.com/ernestio/ernest-config-client"
 	"github.com/nats-io/nats"
 )
 
@@ -74,15 +74,7 @@ func createNat(ev *Event) error {
 }
 
 func main() {
-	natsURI := os.Getenv("NATS_URI")
-	if natsURI == "" {
-		natsURI = nats.DefaultURL
-	}
-
-	nc, natsErr = nats.Connect(natsURI)
-	if natsErr != nil {
-		log.Fatal(natsErr)
-	}
+	nc = ecc.NewConfig(os.Getenv("NATS_URI")).Nats()
 
 	fmt.Println("listening for nat.create.aws")
 	nc.Subscribe("nat.create.aws", eventHandler)
